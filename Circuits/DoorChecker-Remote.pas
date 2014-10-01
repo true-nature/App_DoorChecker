d1d4383a4e47a93afb64996dbe71d18394eb68e7
Yasuhiro Magara <gama@users.sourceforge.jp>
2014-10-02 01:51:31 +0900

    基板レイアウトをPaasで作成してみる。

---
+++ Circuits/DoorChecker-Remote.pas
@@ -0,0 +1,17 @@
+*** Version
+"Version 1.5.2 (20100621)"
+*** Pitch
+1
+*** Board
+"\Board\AKI-C.bmp","\Board\AKI-C_.bmp"
+*** Type
+0
+*** Parts
+"\IC\SOP-28.bmp",270,108,41,72,90
+"",270,149
+"",270,161
+*** BottomLine
+*** TopLine
+*** BottomJump
+*** TopJump
+*** Area
